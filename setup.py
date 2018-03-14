c7ed7727f4db7ef5995edc88fc208c076d24f40f
darkcode357 <darkcode357@gmail.com>
2018-03-14 18:40:15 -0400

    lib brainiac

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ setup(
         'python-nmap',
         'GitPython',
     ],
-  version = '1.0',
-  description='lib voltado para desenvolvimento de ferramentas',
+  version = '3.3',
+  description='lib voltada para o desenvolvimento de ferramentas de pentest',
   author='darkcode0x00',
   author_email='darkcode357@gmail.com',
   license='MIT',
-  url = 'https://github.com/darkcode357/brainiac_pwn_pypi', # use the URL to the github repo
-  download_url = 'https://raw.githubusercontent.com/darkcode357/brainiac_pwn_pypi/master/brainiac-1.0.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/darkcode357/brainiac_pwn', # use the URL to the github repo
+  download_url = 'https://raw.githubusercontent.com/darkcode357/brainiac_pwn_pypi/master/brainiac-1.1.tar.gz', # I'll explain this in a second
 
 )
 
